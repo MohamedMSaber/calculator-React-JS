cbf74c2775025f423d150cfb9e7a361dccdbb541
MohamedMSaber <MohamedMSaber@outlook.com>
2022-10-24 01:21:39 +0200

    Created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+
+
+export default function App() {
+  const [claculation, setClaculation] = useState("");
+  const [result, setResult] = useState('');
+  const operations = ['/','*','+','-','.'];
+
+  function showNums() {
+    const nums = [];
+    for (let i = 1; i < 10; i++) {
+      nums.push(
+        <button 
+        onClick={()=>{updateCalc(i.toString())}}
+        key={i}>{i}</button>
+      )
+    }
+    return nums;
+  }
+
+  function updateCalc(x) {
+    if (
+      operations.includes(x)&&claculation ==='' ||
+      operations.includes(x)&& operations.includes(claculation.slice(-1)) 
+    ) {
+      return;
+    }
+    if (!operations.includes(x)) {
+      setResult(eval(claculation+x).toString())
+    }
+
+    setClaculation(claculation+x)
+  }
+
+  function calc() {
+    setClaculation(eval(claculation).toString());
+  }
+
+  function clearScreen(params) {
+    if (claculation == ''){
+      return;
+    }
+    const vlaue= claculation.slice(0,-1);
+    setClaculation(vlaue);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="calc">
+        <div className="screen">
+          {result ? <span>({result})</span> : ''}{claculation || "0"}
+        </div>
+        <div className="operations">
+          <button onClick={()=>{updateCalc('+')}}>+</button>
+          <button onClick={()=>{updateCalc('-')}}>-</button>
+          <button onClick={()=>{updateCalc('*')}}>*</button>
+          <button onClick={()=>{updateCalc('/')}}>/</button>
+          <button onClick={()=>{clearScreen()}}>DEL</button>
+        </div>
+        <div className="in">
+          {showNums()}
+          <button onClick={()=>{updateCalc('0')}}>0</button>
+          <button onClick={()=>{updateCalc('.')}}>.</button>
+          <button onClick={()=>{calc()}}>=</button>
+        </div>
+
+      </div>
     </div>
   );
 }
 
-export default App;
